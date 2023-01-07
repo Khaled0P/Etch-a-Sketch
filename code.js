28835c1e581fcd6ed33b25e680967d9a20dd862f
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-01-07 23:38:57 +0200

    feat: add reset & gridSize buttons
    add reset button to reset grid on click
    
    add gridSize button(function not specified yet)

--- code.js
+++ code.js
@@ -1,16 +1,26 @@
 const container = document.querySelector('.container');
+let gridSize;
 
 
-
-function createGrid(){
-    for (i = 0; i<16;i++){
-        for(j=0; j<16;j++){
+function createGrid(x =16){
+    for (i = 0; i<x;i++){
+        for(j=0; j<x;j++){
             let gridBox = document.createElement('div');
             gridBox.className = 'gridBox';
             container.appendChild(gridBox);
+            gridBox.addEventListener('mouseover', ()=> {
+                gridBox.style.backgroundColor= 'black';
+            })
         }
     }
 
 }
 
-createGrid();
+function reset(){
+    let gridBox = document.querySelectorAll('.gridBox');
+    gridBox.forEach((box) => {
+        box.style.backgroundColor = 'white';
+    })
+}
+
+createGrid(gridSize);
