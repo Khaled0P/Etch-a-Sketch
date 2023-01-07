2b32b5401f90e38f4b6b936019df79295ae68a36
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-01-08 00:21:15 +0200

    feat: add button to specify grid size

--- code.js
+++ code.js
@@ -1,15 +1,19 @@
 const container = document.querySelector('.container');
-let gridSize;
+let gridSize
 
 
 function createGrid(x =16){
+    let boxWidth = 600/x;
+    let boxHeight = 400/x;
     for (i = 0; i<x;i++){
         for(j=0; j<x;j++){
             let gridBox = document.createElement('div');
             gridBox.className = 'gridBox';
+            gridBox.style.width = `${boxWidth}px`;
+            gridBox.style.height = `${boxHeight}px`;
             container.appendChild(gridBox);
             gridBox.addEventListener('mouseover', ()=> {
-                gridBox.style.backgroundColor= 'black';
+            gridBox.style.backgroundColor= 'black';
             })
         }
     }
@@ -21,6 +25,19 @@ function reset(){
     gridBox.forEach((box) => {
         box.style.backgroundColor = 'white';
     })
+}
+
+function gridSizef(){
+    let gridSize = prompt('enter grid size');
+    while (Number(gridSize) > 100){
+        gridSize = prompt("number must be below 100, enter grid size");
+    }
+        let gridBox = document.querySelectorAll('.gridBox');
+        gridBox.forEach((box) => {
+        box.remove();
+    })
+        createGrid(gridSize);
+
 }
 
 createGrid(gridSize);
